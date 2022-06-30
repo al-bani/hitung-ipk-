f56b4a5ccf2b9722a28e7119451b25752d523315
al_bani <33950918+al-bani@users.noreply.github.com>
2022-06-30 21:02:15 +0700

    Update hitung.php

--- hitung.php
+++ hitung.php
@@ -4,19 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menghitung IPK</title>
+    <title>Menghitung IPK/IPS</title>
 </head>
 <body>
 
 <?php
-    $jml_matkul = 0;
     session_start();
     $jml_matkul = $_GET['jumlah_matkul'];
     $bobot = [];
     $nm_matkul = [];
 
 
-    if ($jml_matkul == 0){
+    if ($_GET['jumlah_matkul'] == NULL){
         session_destroy();
         echo "error";
     }
@@ -29,8 +28,14 @@
         if ($nilai == 'A'){
             $A = 4;
             $bobot[$i] = $jml_sks[$i]*$A;
+        } else if ($nilai == 'AB'){
+            $AB = 3.5;
+            $bobot[$i] = $jml_sks[$i]*$B;
         } else if ($nilai == 'B'){
             $B = 3;
+            $bobot[$i] = $jml_sks[$i]*$B;
+        } else if ($nilai == 'BC'){
+            $BC = 2.5;
             $bobot[$i] = $jml_sks[$i]*$B;
         } else if ($nilai == 'C'){
             $C = 2;
@@ -38,6 +43,9 @@
         } else if ($nilai == 'D'){
             $D = 1;
             $bobot[$i] = $jml_sks[$i]*$D;
+        } else if ($nilai == 'E'){
+            $E = 0;
+            $bobot[$i] = $jml_sks[$i]*$B;
         }
     }
 
@@ -49,12 +57,10 @@
         $total_sks += $jml_sks[$key];
     }
 
-    $IP = $total_bobot/$total_sks;
+    (float) $IP = $total_bobot/$total_sks;
 
 }
 ?>
-
-
 
     <div class="form">
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
@@ -66,16 +72,19 @@
             <label>   Nilai : </label>
             <select name='nilai_matkul[]'>
     		    <option value="A" >A</option>
+                <option value="AB" >AB</option>
 	    	    <option value="B" >B</option>
+                <option value="BC" >BC</option>
                 <option value="C" >C</option>
-		        <option value="D" >D</option>		
+		        <option value="D" >D</option>
+                <option value="E" >E</option>				
 	        </select>
         <br></br>
         <?php } ?>
         </br><input type="submit" name="kirim" value="Hitung">
 
         <?php if(isset($_POST['kirim'])){ ?>
-            <h3>hasil IPK/IPS :  <?php  echo round($IP,2); ?></h3>
+            <h3><?php  echo "Hasil IPK/IPS : ".round((float)$IP,2); ?></h3>
             <p><a href="https://facebook.com/alcopoune.id"> © : alcopoune</a></p>
         <?php } ?>
 
