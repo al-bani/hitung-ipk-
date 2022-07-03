5d31c55118f97300dec3a26cbc05fbb68403f535
al_bani <33950918+al-bani@users.noreply.github.com>
2022-07-03 12:30:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,11 +10,17 @@
 <?php
     session_start();
     $_SESSION['jumlah_matkul'] = $jml_matkul = 0;
+    $_SESSION['tipe_nilai'] = $tipe_n = '';
 ?>
     <form method="get" action="hitung.php">
         <h2>Menghitung IPS/IPK anda</h2>
         <label>Masukkan Jumlah mata kuliah : </label>
         <input type="Number" name="jumlah_matkul" required>
+        <label>Tipe Penilaian : </label>
+        <select name ="tipe_nilai">
+            <option value="num">Nilai angka</option>
+            <option value="abjad">Nilai abjad</option>
+        </select>
         <input type="submit">
     </form>
 </body>
